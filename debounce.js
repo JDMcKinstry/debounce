54208d61e635d9f5bdd3f64325007810c3d4dca7
JD McKinstry <jdmckinstry@gmail.com>
2017-01-16 11:35:30 -0500

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,20 +1,20 @@
-/**	debounce(callback, arguments[], delayTime)	||	debounce.apply(element, [callback, arguments[], delayTime])
- *		Function.debounce(element, arguments[], delayTime)
+/**	debounce(callback, object, delay, arguments)	||	Function.debounce(object, arguments[], delayTime)
+ *	Method to help reduce multifire issues 
  **/
 ;(function() {
-	var debounceTimer;
-	function debounce(cb, args, delayTime) {
-		var $this = this;
+	function debounce(cb, obj, delay, args) {
 		args = 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : [];
-		'number' != typeof delayTime && (delayTime = 0);
-		debounceTimer && clearTimeout(debounceTimer);
-		return debounceTimer = setTimeout(function() { cb.apply($this, args); }, delayTime);
+		(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
+		obj.tmrDebounce && clearTimeout(obj.tmrDebounce);
+		return obj.tmrDebounce = setTimeout(function() { delete obj.tmrDebounce; cb.apply(obj, args); }, delay);
 	}
 	window.hasOwnProperty("debounce") || (window.debounce = debounce);
 	
 	/**	USE THE FOLLOWING WITH CAUTION || REMOVE IF YOU DON'T WANT TO APPEND TO FUNCTION OBJECT **/
-	function funcDebounce($this, args, delayTime) { return debounce.apply($this, [this, args, delayTime]); }
-	Object['defineProperty'] && !Function.prototype.hasOwnProperty('funcDebounce')
-		? Object.defineProperty(Function.prototype, 'debounce', { value: funcDebounce })
-			: Function.prototype.debounce = funcDebounce;
+	function funcDebounce(obj, delay, args) { return debounce(this, obj, delay, args); }
+	if (window.debounce === debounce) {
+		Object['defineProperty'] && !Function.prototype.hasOwnProperty('funcDebounce')
+			? Object.defineProperty(Function.prototype, 'debounce', { value: funcDebounce })
+				: Function.prototype.debounce = funcDebounce;
+	}
 })();
