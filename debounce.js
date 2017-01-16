36d1079297dd91702d61f69986ae3804ba3e88df
JD McKinstry <jdmckinstry@gmail.com>
2017-01-16 12:18:54 -0500

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,16 +1,17 @@
-/**	debounce(callback, object, delay, arguments)	||	Function.debounce(object, arguments[], delayTime)
+/**	debounce(callback, arguments, delay, object)	||	Function.debounce(arguments, delay, object)
  *	Method to help reduce multifire issues 
  **/
 ;(function() {
-	function debounce(cb, obj, delay, args) {
-		args = 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : [];
-		(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
+	function debounce(cb, args, delay, obj) {
+		void 0 == obj && (obj = window);
 		obj['__debounce_timer__'] && clearTimeout(obj['__debounce_timer__']);
+		args = 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : arguments;
+		(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
 		return obj['__debounce_timer__'] = setTimeout(function() { delete obj['__debounce_timer__']; cb.apply(obj, args); }, delay);
 	}
 	window.hasOwnProperty("debounce") || (window.debounce = debounce);
 	/**	CAUTION	APPENDS TO FUNCTION OBJECT **/
-	function funcDebounce(obj, delay, args) { return debounce(this, obj, delay, args); }
+	function funcDebounce(args, delay, obj) { return debounce(this, args, delay, obj); }
 	if (window.debounce === debounce) {
 		Object['defineProperty'] && !Function.prototype.hasOwnProperty('funcDebounce')
 			? Object.defineProperty(Function.prototype, 'debounce', { value: funcDebounce })
