bdd90d6e5f46c3e1ff20fc57d63fcb07af369b01
JD McKinstry <jdmckinstry@gmail.com>
2017-01-16 11:40:35 -0500

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -5,12 +5,11 @@
 	function debounce(cb, obj, delay, args) {
 		args = 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : [];
 		(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
-		obj.tmrDebounce && clearTimeout(obj.tmrDebounce);
-		return obj.tmrDebounce = setTimeout(function() { delete obj.tmrDebounce; cb.apply(obj, args); }, delay);
+		obj['__debounce_timer__'] && clearTimeout(obj['__debounce_timer__']);
+		return obj['__debounce_timer__'] = setTimeout(function() { delete obj['__debounce_timer__']; cb.apply(obj, args); }, delay);
 	}
 	window.hasOwnProperty("debounce") || (window.debounce = debounce);
-	
-	/**	USE THE FOLLOWING WITH CAUTION || REMOVE IF YOU DON'T WANT TO APPEND TO FUNCTION OBJECT **/
+	/**	CAUTION	APPENDS TO FUNCTION OBJECT **/
 	function funcDebounce(obj, delay, args) { return debounce(this, obj, delay, args); }
 	if (window.debounce === debounce) {
 		Object['defineProperty'] && !Function.prototype.hasOwnProperty('funcDebounce')
