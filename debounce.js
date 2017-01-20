b8d65cd865a0ff1a8c61d7b39d9839cb3fa78c6c
JD McKinstry <jdmckinstry@gmail.com>
2017-01-20 11:08:35 -0500

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,21 +1,18 @@
-/**	debounce(callback, arguments, delay, object)	||	Function.debounce(arguments, delay, object)	||	lib.debounce(cb, args, delay, object=lib)
+/**	debounce(callback, arguments, delay, object)
  *	Method to help reduce multifire issues 
  **/
 ;(function() {
-	function debounce(cb, args, delay, obj) {
-		void 0 == obj && (obj = window);
-		obj['__debounce_timer__'] && clearTimeout(obj['__debounce_timer__']);
-		args = 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : arguments;
-		(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
-		return obj['__debounce_timer__'] = setTimeout(function() { delete obj['__debounce_timer__']; cb.apply(obj, args); }, delay);
+	function debounce(cb, args, delay, obj) {	//	return function
+		var tmr;
+		return function() {
+			void 0 == obj && (obj = this);
+			tmr && clearTimeout(tmr);
+			args = args && 'object' == typeof args ? Array.prototype.slice.call(args) : void 0 != args ? [args] : arguments;
+			(0 > delay || "number" != typeof delay) && (delay = 250);	//	250 milliseconds as a default delay time
+			tmr = setTimeout(function() { cb.apply(obj, args); }, delay);
+		}
 	}
 	window.hasOwnProperty("debounce") || (window.debounce = debounce);
-	/**	CAUTION	APPENDS TO FUNCTION OBJECT **/
-	/**	Function based libs (like jQuery) will be lib.debounce(cb, args, delay, object=lib)	**/
-	function funcDebounce(args, delay, obj) { return args instanceof Function ? debounce(args, delay, obj, void 0 != arguments[3] ? arguments[3] : this) : debounce(this, args, delay, obj); }
-	if (window.debounce === debounce) {
-		Object['defineProperty'] && !Function.prototype.hasOwnProperty('funcDebounce')
-			? Object.defineProperty(Function.prototype, 'debounce', { value: funcDebounce })
-				: Function.prototype.debounce = funcDebounce;
-	}
+	//	Add to jQuery if available
+	if (window.hasOwnProperty('jQuery') && !jQuery['debounce']) jQuery.debounce = debounce;
 })();
